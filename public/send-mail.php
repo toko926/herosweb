19d6ab4f67d39ec1f304b2f47e9dbf7675aad75c
toko926 <satorutokobito@gmail.com>
2018-08-10 22:02:46 +0900

    updated a lot

--- public/send-mail.php
+++ public/send-mail.php
@@ -1,6 +1,6 @@
 <?php
 
-$to = 'user@yourdomain.com'; // Change your email address
+$to = 'info@heros.studio'; // Change your email address
 
 
 $name = $_POST['name'];
@@ -12,7 +12,7 @@ $message = $_POST['message'];
 // Email Submit
 // Note: filter_var() requires PHP >= 5.2.0
  if ( isset($email) && isset($name) && isset($subject) && isset($message) && filter_var($email, FILTER_VALIDATE_EMAIL) ) {
- 
+
   // detect & prevent header injections
   $test = "/(content-type|bcc:|cc:|to:)/i";
   foreach ( $_POST as $key => $val ) {
@@ -23,7 +23,7 @@ $message = $_POST['message'];
 
 $body = <<<EMAIL
 subject : $subject
-  
+
 My name is, $name.
 
 $message
@@ -32,8 +32,8 @@ From : $name
 Email : $email
 
 EMAIL;
-  
-  
+
+
 $header = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
     'Reply-To: ' . $_POST["email"] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
@@ -42,6 +42,6 @@ $header = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
  // mail( $to , $_POST['subject'], $_POST['message'], $headers );
  mail($to, $subject, $body, $header);
   //      ^
-  //  Replace with your email 
+  //  Replace with your email
 }
 ?>
